cb4096365d28f0f10702417bab528bc0c00c6dcd
tanmaym150 <tanmaymahamulkar150@gmail.com>
2021-08-18 10:47:37 +0530

    Add project files.

---
+++ MulticastDelegate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MulticastDelegate
+{   //multicast delegates
+    public delegate void MultiDelegate();
+    class SayHi
+    {
+        static public void SayHello()
+        {
+            Console.WriteLine("Helloo..");
+        }
+        static public void SayHola()
+        {
+            Console.WriteLine("Hola...");
+        }
+        static public void SayCiao()
+        {
+            Console.WriteLine("Ciao..");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MultiDelegate md1 = new MultiDelegate(SayHi.SayHello);
+            MultiDelegate md2 = new MultiDelegate(SayHi.SayHola);
+            MultiDelegate md3 = new MultiDelegate(SayHi.SayCiao);
+            md1();
+            md2();
+            md3();
+
+
+        }
+    }
+}
